31204be4b09080399aa224300c0a5a760995f989
dynameek <nathanoguntuberu@gmail.com>
2017-07-15 09:01:57 +0100

    included credential.php to keep password secret. changed output to html table and made the page responsive

--- get_users.php
+++ get_users.php
@@ -1,21 +1,37 @@
-<?php
-	$db_handle = mysqli_connect("onathan.5gbfree.com", "nate", "nathaneil.1995", "bootcamp");
-	if(!$db_handle){
-		$returnValue = false;
-	}else{
-		$sql = "SELECT * FROM students";
-		$query = mysqli_query($db_handle, $sql);
-		if(!$query){
-			$returnValue = false;
-		}else{
-			$num_rows = mysqli_num_rows($query);
-			for($i = 0; $i < $num_rows; $i++){
-			    $records[$i] = mysqli_fetch_assoc($query);
-				echo $records[$i]['student_firstname']." ".$records[$i]['student_lastname']." ".$records[$i]['student_level']." ".$records[$i]['student_course']." ".$records[$i]['student_phone']."<br>";
-				$returnValue = true;
+<!DOCTYPE html>
+	<head>
+		<title>Students</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	</head>
+	<body>
+		<!-- Code to get users -->
+		<?php
+			#
+			include_once('../credentials.php');
+			#
+			$db_handle = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+			#
+			if(!$db_handle){
+				$returnValue = false;
+			}else{
+				$sql = "SELECT * FROM students";
+				$query = mysqli_query($db_handle, $sql);
+				if(!$query){
+					$returnValue = false;
+				}else{
+					$num_rows = mysqli_num_rows($query);
+					echo "<table><tr><td>Student Name</td><td>Student level</td><td> Student Course</td><td>Student Phone number</td></tr>";
+					for($i = 0; $i < $num_rows; $i++){
+						$records[$i] = mysqli_fetch_assoc($query);
+						echo "<tr>"; 
+						echo "<td>".$records[$i]['student_firstname']." ".$records[$i]['student_lastname']."</td><td> ".$records[$i]['student_level']." </td><td>".$records[$i]['student_course']."</td><td> ".$records[$i]['student_phone']."</td><br>";
+						echo "</tr>";
+						$returnValue = true;
+					}
+					echo "<table";
+				}
 			}
-		}
-	}
-	echo $returnValue;
-	mysqli_close($db_handle);
-?>
+			#
+			mysqli_close($db_handle);
+		?>
+	</body>
